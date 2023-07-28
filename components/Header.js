856691002497a51c91aa7801abf73ca53f95098c
cristinasomlea <cristinamarta.somlea@gmail.com>
2023-07-28 18:22:03 +0300

    The search bar template has been added.

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import React from "react";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className=" pl-2 flex items-center justify-between max-w-6xl">
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
@@ -19,6 +20,14 @@ export default function Header() {
           className="object-contain"
         ></Image>
       </div>
+
+      <div className="relative mt-1">
+        <div className="absolute top-2">
+          <SearchIcon className="h-5 text-gray-500 pl-2"></SearchIcon>
+        </div>
+        <input type="text" placeholder="Search" className="bg-gray-50 pl-8 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"/>
+      </div>
+
       <div>
         <h1>Right Side</h1>
       </div>
